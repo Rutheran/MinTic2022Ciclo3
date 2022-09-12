70b0549245fb1ea58211a1adca4ff831cf4a26ea
SandraM <silane611@gmail.com>
2022-09-11 19:15:34 -0500

    cree la clase Medico

--- MISClinica/clinicaAPI/models.py
+++ MISClinica/clinicaAPI/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Medico(models.Model):
+    id = models.AutoField(primary_key=True)
+    paciente = models.ForeignKey(Persona, related_name='medico', on_delete=models.CASCADE)
+    registro = models.ForeingKey(Registro, related_name='medico', on_delete=models.CASCADE)
+
+
--- MISClinica/clinicaAPI/views.py
+++ MISClinica/clinicaAPI/views.py
@@ -1,3 +1,21 @@
+import json
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed
 
 # Create your views here.
+def newMedico(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            medico = newMedico(
+                id = data["id"],
+                paciente = data["paciente"],
+                registro = data["registro"]
+            )
+            medico.save()
+            return HttpResponse("Nuevo medico agregado")
+        except:
+            return HttpResponseBadRequest("Error en los datos enviados")
+    else:
+        return HttpResponseNotAllowed(['POST'], "Método inválido")
+
