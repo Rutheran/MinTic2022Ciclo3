8bfe59a6d5d79ff822c389851318c9caad7778ff
Ricardo Mazuera <ricardo.mazuera@gmail.com>
2022-09-11 20:13:54 -0500

    Se corrige el error en la creación de las tablas

--- MISClinica/clinicaAPI/models.py
+++ MISClinica/clinicaAPI/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
-
-class Medico(models.Model):
-    id = models.AutoField(primary_key=True)
-    paciente = models.ForeignKey(Persona, related_name='medico', on_delete=models.CASCADE)
-    registro = models.ForeingKey(Registro, related_name='medico', on_delete=models.CASCADE)
-
 class Persona(models.Model):
     id = models.BigIntegerField(primary_key=True)
     firstName = models.CharField(max_length=50)
@@ -15,18 +9,21 @@ class Persona(models.Model):
     
 class Paciente(models.Model):
     id = models.AutoField(primary_key= True)
-    persona = models.ForeignKey(Persona, related_name='paciente', on_delete=models.CASCADE)
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
     address = models.CharField(max_length=30)
     city = models.CharField(max_length=20)
     birthday = models.DateField()
-    latitude = models.DecimalField()
-    longitude = models.DecimalField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
 
 class Familiar (models.Model):
     familiar = models.AutoField(primary_key=True)
-    persona = models.ForeignKey(Persona, related_name='persona', on_delete=models.CASCADE)
-    paciente = models.ForeignKey(Paciente, related_name='paciente', on_delete=models.CASCADE)
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
     parentesco = models.CharField(max_length=15)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField()
 
-
+class Medico(models.Model):
+    id = models.AutoField(primary_key=True)
+    paciente = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    # registro = models.ForeingKey(Registro, related_name='medico', on_delete=models.CASCADE)
