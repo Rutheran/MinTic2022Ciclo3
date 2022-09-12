c8bdd80a20e1fba2401f5bce7c61f2769e6bb411
Rutheran <rvtheran@gmail.com>
2022-09-11 19:15:30 -0500

    Tabla Paciente

--- MISClinica/clinicaAPI/models.py
+++ MISClinica/clinicaAPI/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Paciente(models.Model):
+    id = models.AutoField(primary_key= True)
+    persona = models.ForeignKey(Persona, related_name='paciente', on_delete=models.CASCADE)
+    address = models.CharField(max_length=30)
+    city = models.CharField(max_length=20)
+    birthday = models.DateField()
+    latitude = models.DecimalField()
+    longitude = models.DecimalField()
--- MISClinica/clinicaAPI/views.py
+++ MISClinica/clinicaAPI/views.py
@@ -1,3 +1,24 @@
+import json
+from .models import Paciente
 from django.shortcuts import render
+from http.client import HTTPResponse, HttpResponseBadRequest, HttpResponseNotAllowed
 
 # Create your views here.
+def newPaciente(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            paciente = Paciente(
+                id_paciente = data["id_paciente"],
+                address = data["address"],
+                city = data["city"],
+                birthday = data["birthday"],
+                latitude = data["latitude"],
+                longitude = data["longitude"],
+            )
+            paciente.save()
+            return HTTPResponse("Nuevo Paciente Agregado")
+        except:
+            return HttpResponseBadRequest("Error en los datos enviados")
+    else:
+        return HttpResponseNotAllowed(['POST'],"Método Inválido")
