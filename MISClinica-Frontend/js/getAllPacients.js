1395f037c62acd9561e2aa1152c001c2a22c4e89
Ricardo Mazuera <ricardo.mazuera@gmail.com>
2022-09-20 22:31:53 -0500

    finalizado get unico paciente

--- MISClinica-Frontend/js/getAllPacients.js
+++ MISClinica-Frontend/js/getAllPacients.js
@@ -34,7 +34,7 @@ function handlePacientes() {
             <dl>
                 <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                     <dt class="text-sm font-medium text-gray-500">Documento de identidad:</dt>
-                    <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0"> ${pasc.id}</dd>
+                    <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0"> ${pasc.dni}</dd>
                 </div>
                 <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                     <dt class="text-sm font-medium text-gray-500">Celular:</dt>
